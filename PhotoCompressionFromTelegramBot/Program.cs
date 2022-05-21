f2adaa37da231a1fedc3c9e6568fce4ba91824dd
Roman Azarov <azarovrom9215@gmail.com>
2022-05-21 18:42:44 +0300

    first start

--- PhotoCompressionFromTelegramBot/Program.cs
+++ PhotoCompressionFromTelegramBot/Program.cs
@@ -2,28 +2,72 @@
 using Telegram.Bot.Extensions.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types.InputFiles;
+using Aspose.Imaging;
+using Aspose.Imaging.ImageOptions;
+using Telegram.Bot.Types.Enums;
+using Aspose.Imaging.FileFormats.Jpeg;
 
 namespace PhotoCompressionFromTelegramBot
 {
 
     class Program
     {
-        static string token = Environment.GetEnvironmentVariable("TG");
+        static string token = Environment.GetEnvironmentVariable("TG_TOKEN");
 
         static ITelegramBotClient bot = new TelegramBotClient(token);
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
             // Некоторые действия
             Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
-            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            if (update.Type == UpdateType.Message)
             {
                 var message = update.Message;
-                if (message.Text.ToLower() == "/start")
+                if (message.Text != null && message.Text.ToLower() == "/start")
                 {
                     await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать на борт, добрый путник!");
+                    await botClient.SendTextMessageAsync(message.Chat, "Отправь мне фото и я заставлю его похудеть!");
                     return;
                 }
-                await botClient.SendTextMessageAsync(message.Chat, "Привет-привет!!");
+                if (message.Photo is not null)
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, "Ща все буит");
+
+                    var fileId = message.Photo.Last().FileId;
+                    var fileInfo = await botClient.GetFileAsync(fileId);
+                    var filePath = fileInfo.FilePath;
+                    double firstSize = Convert.ToDouble(fileInfo.FileSize);
+                    Console.WriteLine(firstSize);
+
+                    double secondSize = 0;
+                    var dataDir = @"C:\Users\azaro\Desktop\photo.jpeg";
+
+                    //string destinationFilePath = $"../downloaded.file";
+                    await using FileStream fileStream = System.IO.File.OpenWrite(dataDir);
+                    await botClient.DownloadFileAsync(
+                        filePath: filePath,
+                        destination: fileStream);
+                    fileStream.Close();
+
+                    using (Image image = Image.Load(dataDir))
+                    {
+                        JpegOptions options = new JpegOptions();
+                        options.CompressionType = JpegCompressionMode.Progressive;
+                        image.Save(@"C:\Users\azaro\Desktop\photo_out.jpg", options);
+                    }
+
+
+                    using (var file = new FileStream(@"C:\Users\azaro\Desktop\photo_out.jpg", FileMode.Open, FileAccess.Read, FileShare.Read))
+                    {
+                        secondSize = file.Length;
+                        string size = Convert.ToString(100 - ((secondSize / firstSize) * 100));
+                        await bot.SendPhotoAsync(
+                            chatId: message.Chat.Id,
+                            photo: new InputOnlineFile(file),
+                            caption: size);
+                    }
+                    return;
+                }
             }
         }
 
@@ -40,10 +84,12 @@ namespace PhotoCompressionFromTelegramBot
 
             var cts = new CancellationTokenSource();
             var cancellationToken = cts.Token;
+
             var receiverOptions = new ReceiverOptions
             {
                 AllowedUpdates = { }, // receive all update types
             };
+
             bot.StartReceiving(
                 HandleUpdateAsync,
                 HandleErrorAsync,
